6c9f4266804a2dab639e192516d2176c6d11a047
Nguyen Xuan Hoang <hoang170604@gmail.com>
2026-04-05 15:49:04 +0700

    map Food

--- main/src/main/java/com/crossapplication/main/mapper/FoodMapper.java
+++ main/src/main/java/com/crossapplication/main/mapper/FoodMapper.java
@@ -1,8 +1,17 @@
 package com.crossapplication.main.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.crossapplication.main.dto.FoodDTO;
+import com.crossapplication.main.entity.Food;
 
 @Mapper(componentModel = "spring")
 public interface FoodMapper {
-    
+    FoodDTO toDTO(Food entity);
+
+    @Mapping(target="id", ignore=true)
+    @Mapping(target="category", ignore=true)
+
+    Food toEntity(FoodDTO dto);
 }
